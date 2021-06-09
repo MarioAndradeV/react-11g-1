8d7229b632f6381f0d28f419705619047939c9c9
David Moranchel <david.arcshtag@gmail.com>
2021-06-08 20:10:25 -0500

    feat: dynamic data

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,37 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import AppCard from './AppCard';
+import AppCard from "./AppCard";
 
 class Posts extends React.Component {
-    render() {
-        return (
-            <ul className="d-flex flex-column">
-                <li>
-                    <AppCard>
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </AppCard>
-                </li>
+  render() {
+    return (
+      <ul className="d-flex flex-column">
+        <li>
+          <AppCard>
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </AppCard>
+        </li>
 
-                <li>
-                    <AppCard>
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </AppCard>
-                </li>
+        <li>
+          <AppCard>
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </AppCard>
+        </li>
 
-                <li>
-                    <AppCard>
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </AppCard>
-                </li>
-            </ul>
-        )
-    }
+        <li>
+          <AppCard>
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </AppCard>
+        </li>
+      </ul>
+    );
+  }
 }
 
-export default Posts
+export default Posts;
--- src/screens/Exercises/index.jsx
+++ src/screens/Exercises/index.jsx
@@ -1,49 +1,99 @@
 import React, { useState } from "react";
 
+// export default function Exercises() {
+//   const [USD, setUSD] = useState(null);
+//   const [MXN, setMXN] = useState(null);
+
+//   return (
+//     <div className="container h-100">
+//       <div className="row h-100 justify-content-center align-items-center">
+//         <div className="d-flex col-10 col-md-6 bg-dark rounded h-50 align-items-center px-0">
+//           <div className="col p-5">
+//             <form>
+//               <div className="form-group col-12">
+//                 <label className="text-white" htmlFor="USDInput">
+//                   USD
+//                 </label>
+//                 <input
+//                   type="number"
+//                   className="form-control"
+//                   id="USDInput"
+//                   aria-describedby="emailHelp"
+//                   placeholder="Enter USD"
+//                   value={Number(USD)}
+//                   onChange={(event) => {
+//                     setUSD(event.target.value);
+//                     setMXN((event.target.value * 19.84).toFixed(2));
+//                   }}
+//                 />
+//               </div>
+//               <div className="form-group col-12">
+//                 <label className="text-white" htmlFor="MXNInput">
+//                   MXN
+//                 </label>
+//                 <input
+//                   type="number"
+//                   className="form-control"
+//                   id="MXNInput"
+//                   placeholder="Enter MXN"
+//                   value={Number(MXN)}
+//                   onChange={({ target: { value } }) => {
+//                     setUSD((value / 19.84).toFixed(2));
+//                     setMXN(value);
+//                   }}
+//                 />
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+const petsData = [
+  {
+    name: "Sr. Peabody",
+    age: "5",
+  },
+  {
+    name: "Misifu",
+    age: "1",
+  },
+  {
+    name: "Solovino",
+    age: "10",
+  },
+  {
+    name: "Nefermishi",
+    age: "5",
+  },
+];
+
 export default function Exercises() {
-  const [USD, setUSD] = useState(null);
-  const [MXN, setMXN] = useState(null);
+  const buildLIPets = ({ name, age }, index) => (
+    <li key={index} className="list-group-item">
+      Name: {name}, age: {age}
+    </li>
+  );
+
+  const petsUI = petsData.map(({ name, age }, index) => (
+    <li key={index} className="list-group-item">
+      Name: {name}, age: {age}
+    </li>
+  ));
 
   return (
-    <div className="container h-100">
-      <div className="row h-100 justify-content-center align-items-center">
-        <div className="d-flex col-10 col-md-6 bg-dark rounded h-50 align-items-center px-0">
-          <div className="col p-5">
-            <form>
-              <div className="form-group col-12">
-                <label className="text-white" htmlFor="USDInput">
-                  USD
-                </label>
-                <input
-                  type="number"
-                  className="form-control"
-                  id="USDInput"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter USD"
-                  value={Number(USD)}
-                  onChange={(event) => {
-                    setUSD(event.target.value);
-                    setMXN((event.target.value * 19.84).toFixed(2));
-                  }}
-                />
-              </div>
-              <div className="form-group col-12">
-                <label className="text-white" htmlFor="MXNInput">
-                  MXN
-                </label>
-                <input
-                  type="number"
-                  className="form-control"
-                  id="MXNInput"
-                  placeholder="Enter MXN"
-                  value={Number(MXN)}
-                  onChange={({ target: { value } }) => {
-                    setUSD((value / 19.84).toFixed(2));
-                    setMXN(value);
-                  }}
-                />
-              </div>
-            </form>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col">
+          <div className="card" style={{ width: "18rem" }}>
+            <div className="card-header">Pets</div>
+            <ul className="list-group list-group-flush">
+              {petsData.map(buildLIPets)}
+            </ul>
+            <div className="card-header">Pets</div>
+            <ul className="list-group list-group-flush">{petsUI}</ul>
           </div>
         </div>
       </div>
