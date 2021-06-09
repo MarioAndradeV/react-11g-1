14bb73fb71434e7df1665c01b0c8e5e13beec808
MarioAndrade <yomariolalo@hotmail.com>
2021-06-09 17:50:26 -0500

    Credit Card

--- src/screens/Exercises/index.jsx
+++ src/screens/Exercises/index.jsx
@@ -51,6 +51,107 @@ import Pet from "../../components/Pet";
 //   );
 // }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Ejercicio credit card
+
+export default function Exercises() {
+  const [cardNumber, setNumber] = useState("");
+  const [cardName, setName] = useState("");
+  const [cardCVC, setCVC] = useState("");
+  const [cardDate, setDate] = useState("");
+
+  return (
+    <div className="container h-100">
+      <div className="row h-100  align-items-center">
+        <div className="d-flex col-10 col-md-6 bg-danger rounded h-50 align-items-center px-0">
+          <div className="col p-4">
+            <form>
+              <h1 className="text-white">Credit Card</h1>
+              <div className="form-group col-12">
+                <label className="text-white" htmlFor="cardNumberInput">
+                  Card Number
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="cardNumberInput"
+                  placeholder="Ej. XXXX-XXXX-XXXX-XXXX"
+                  value={cardNumber}
+                  onChange={(event) => {
+                    setNumber(event.target.value);
+                  }}
+                  maxLength="19"
+                />
+              </div>
+              <div className="form-group col-12">
+                <label className="text-white" htmlFor="cardNameInput">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="cardNameInput"
+                  placeholder="Ej. Mario Andrade..."
+                  value={cardName}
+                  onChange={(event) => {
+                    setName(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="form-group col-12">
+                <label className="text-white" htmlFor="cardCVCInput">
+                  CVC
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="cardCVCInput"
+                  placeholder="Ej. 123"
+                  value={cardCVC}
+                  onChange={(event) => {
+                    setCVC(event.target.value);
+                  }}
+                  maxLength="3"
+                />
+              </div>
+              <div className="form-group col-12">
+                <label className="text-white" htmlFor="cardDateInput">
+                  Date
+                </label>
+                <input
+                  type="month"
+                  className="form-control"
+                  id="cardDateInput"
+                  placeholder=""
+                  value={cardDate}
+                  onChange={(event) => {
+                    setDate(event.target.value);
+                  }}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="card h-50 w-50 bg-info">
+          <div className="card-body">
+            <div className=" d-flex justify-content-between ">
+              <h1 className="text-white">VISA</h1>
+            </div>
+            <h2 className="text-white ">{cardNumber}</h2>
+            <h2 className="text-white">{cardName}</h2>
+            <div className="d-flex justify-content-between mt-2">
+              <h3 className="text-white mt-6">{cardCVC}</h3>
+              <h3 className="text-white">{cardDate}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // const petsData = [
 //   {
 //     name: "Sr. Peabody",
@@ -70,8 +171,6 @@ import Pet from "../../components/Pet";
 //   },
 // ];
 
-<<<<<<< HEAD
-=======
 // export default function Exercises() {
 //   const buildLIPets = ({ name, age }, index) => (
 //     <li key={index} className="list-group-item">
@@ -103,82 +202,66 @@ import Pet from "../../components/Pet";
 //   );
 // }
 
->>>>>>> 95bc4a0434a29b10e7abcb9fe2bddbb423ef41b8
-const petsData = {
-  dogs: [
-    {
-      name: "Sr. Peabody",
-      age: "5",
-    },
-    {
-      name: "Solovino",
-      age: "10",
-    },
-  ],
-  cats: [
-    {
-      name: "Misifu",
-      age: "1",
-    },
-    {
-      name: "Nefermishi",
-      age: "5",
-    },
-  ],
-<<<<<<< HEAD
-};
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-export default function Exercises() {
-  const a = Object.keys(petsData);
+//Ejercicio Pets con objeto de arrays
 
-  const buildLIPets = ({ name, age }, index) => (
-    <li key={index} className="list-group-item">
-      Name: {name}, age: {age}
-    </li>
-  );
+// const petsData = {
+//   dogs: [
+//     {
+//       name: "Sr. Peabody",
+//       age: "5",
+//     },
+//     {
+//       name: "Solovino",
+//       age: "10",
+//     },
+//   ],
+//   cats: [
+//     {
+//       name: "Misifu",
+//       age: "1",
+//     },
+//     {
+//       name: "Nefermishi",
+//       age: "5",
+//     },
+//   ],
+//   parrots: [
+//     {
+//       name: "Garzilazo",
+//       age: "1",
+//     },
+//     {
+//       name: "Pancho",
+//       age: "5",
+//     },
+//   ],
+// };
 
-  const petsUI = petsData.map(({ name, age }, index) => (
-    <li key={index} className="list-group-item">
-      Name: {name}, age: {age}
-    </li>
-  ));
+// export default function Exercises() {
+//   const buildPetType = ([type, pets], index) => (
+//     <React.Fragment key={index}>
+//       <div className="card-header">{type}</div>
+//       <ul className="list-group list-group-flush">
+//         {pets.map((pet, index) => (
+//           <Pet pet={pet} key={index} index={index} />
+//         ))}
+//       </ul>
+//     </React.Fragment>
+//   );
 
-  //Ejercicio 2
-=======
-  parrots: [
-    {
-      name: "Garzilazo",
-      age: "1",
-    },
-    {
-      name: "Pancho",
-      age: "5",
-    },
-  ],
-};
+//   return (
+//     <div className="container">
+//       <div className="row justify-content-center">
+//         <div className="col">
+//           <div className="card" style={{ width: "18rem" }}>
+//             {Object.entries(petsData).map(buildPetType)}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default function Exercises() {
-  const buildPetType = ([type, pets], index) => (
-    <React.Fragment key={index}>
-      <div className="card-header">{type}</div>
-      <ul className="list-group list-group-flush">
-        {pets.map((pet, index) => (
-          <Pet pet={pet} key={index} index={index} />
-        ))}
-      </ul>
-    </React.Fragment>
-  );
-
->>>>>>> 95bc4a0434a29b10e7abcb9fe2bddbb423ef41b8
-  return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col">
-          <div className="card" style={{ width: "18rem" }}>
-            {Object.entries(petsData).map(buildPetType)}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
